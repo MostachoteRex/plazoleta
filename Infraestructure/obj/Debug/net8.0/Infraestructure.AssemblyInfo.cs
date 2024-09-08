c0fe0eb98a60b6428ac83bf804dcd8f36fb71c1a
Mostachote Rex <luisaj512@hotmail.com>
2024-09-07 19:02:14 -0500

    actualizacion parte backen

--- Infraestructure/obj/Debug/net8.0/Infraestructure.AssemblyInfo.cs
+++ Infraestructure/obj/Debug/net8.0/Infraestructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f561b5a1d23b73d05435eceba79cad5431c8ba7a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
