b53f81b768e95ad2e5ea53e8f701c394d2dd1195
Mostachote Rex <luisaj512@hotmail.com>
2024-09-24 08:09:02 -0500

    Primera actualizacion del backend

--- Infraestructure/obj/Debug/net8.0/Infraestructure.AssemblyInfo.cs
+++ Infraestructure/obj/Debug/net8.0/Infraestructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f561b5a1d23b73d05435eceba79cad5431c8ba7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0fe0eb98a60b6428ac83bf804dcd8f36fb71c1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Infraestructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
